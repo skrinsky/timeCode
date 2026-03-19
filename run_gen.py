88e0657b6b9e66aa13392b1d9ef5f9901d8d263d
skrinsky <skrinsky@umich.edu>
2026-03-18 23:52:27 -0700

    Update run scripts for Stage 2 (all instruments, resume from checkpoint)

---
+++ run_gen.py
@@ -0,0 +1,3 @@
+from timecode_audio.data.synthetic_gen import generate_dataset
+
+generate_dataset('data/synthetic', n_clips=500_000, stage='stage2')
--- run_train.py
+++ run_train.py
@@ -2,6 +2,8 @@ from timecode_audio.data.synthetic_gen import SyntheticDataset
 from timecode_audio.training.config import TrainingConfig
 from timecode_audio.training.trainer import Trainer
 
-config = TrainingConfig(total_steps=50_000)
+config = TrainingConfig(total_steps=200_000)
 dataset = SyntheticDataset('data/synthetic')
-Trainer(config, dataset).train()
+trainer = Trainer(config, dataset)
+trainer.load_checkpoint('checkpoints/best.pt')
+trainer.train()
